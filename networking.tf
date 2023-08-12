092cfb4c2af6f223424ce32592d59ea49c7f4fa6
deleonab <delesodade@hotmail.com>
2023-08-12 10:31:58 +0100

    added gitignore to repo

--- networking.tf
+++ networking.tf
@@ -12,11 +12,11 @@ resource "aws_subnet" "public" {
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "Main"
+    Name = "Main_public_subnet"
   }
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
@@ -24,16 +24,22 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-resource "aws_route_table" "r" {
+resource "aws_route_table" "rtb-public" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  
   tags = {
-    Name = "main_route_table"
+    Name = "Public-RT"
   }
+}
+
+
+resource "aws_route" "rtb-public-route" {
+  route_table_id         = aws_route_table.rtb-public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.rtb-public.id
 }
---
+++ security.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "my_instance_SG" {
+  name_prefix = "example-"
+
+  # Allow incoming SSH (port 22) and HTTP (port 80) traffic
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
